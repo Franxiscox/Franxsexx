fe54b35d6602879d648b45f0f049c7a7c01282bd
Fran Stanley <89065588+Franxiscox@users.noreply.github.com>
2024-07-29 22:05:54 -0400

    Actualizar propietario(a)-update.js

--- plugins/propietario(a)-update.js
+++ plugins/propietario(a)-update.js
@@ -5,7 +5,7 @@ if (global.conn.user.jid == conn.user.jid) {
 let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
 conn.reply(m.chat, stdout.toString(), m)}
 //} catch {
-var update = execSync('git remote set-url origin https://github.com/Wilsmac/NatsukiBot.git && git pull')
+var update = execSync('git remote set-url origin https://github.com/Franxiscox/Franxsexx.git && git pull')
 await m.reply(update.toString())
 } catch {
 await m.reply(`${fg}`) 
